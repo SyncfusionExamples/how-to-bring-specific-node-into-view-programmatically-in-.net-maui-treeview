537583d24525ff4bff3ef37e25944bafcb999db3
DhivyaSF4094 <dhivya.sf4094@gmail.com>
2023-09-12 11:39:01 +0530

    Aligned the line space in Windows/App.xaml.cs

--- TreeViewMaui/Platforms/Windows/App.xaml.cs
+++ TreeViewMaui/Platforms/Windows/App.xaml.cs
@@ -17,9 +17,8 @@ public partial class App : MauiWinUIApplication
 	/// </summary>
 	public App()
 	{
-        this.InitializeComponent();
-    }
-
+		this.InitializeComponent();
+	}
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
 
